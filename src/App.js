3d203bf0bb2656c5a65de87aba3f17da37690bf1
Amandeep Bassi <asbassi@outlook.com>
2021-07-01 11:28:59 +0100

    Created basic page structure for movie finder page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavSection from './Components/NavSection';
+import Container from 'react-bootstrap/Container';
+import Home from './Components/Home/Home'
+import Finder from './Components/FinderComponents/Finder'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavSection />
+      <Switch>
+        <Route exact path="/">
+          <Container>
+            <Home />
+          </Container>
+        </Route>
+        <Route path="/finder">
+          <Container>
+            <Finder />
+          </Container>
+        </Route>
+      </Switch>
+    </Router >
   );
 }
 
