f7187253a4a444896a869272fbfb197fb1b5e77b
Amandeep Bassi <asbassi@outlook.com>
2021-07-01 11:36:36 +0100

    Created input field for movie search feature

--- src/Components/FinderComponents/Finder.jsx
+++ src/Components/FinderComponents/Finder.jsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react'
+import Form from 'react-bootstrap/Form'
 import React from 'react'
 
 const Finder = () => {
+
+    const [loaded, setLoaded] = useState(false);
+    const [error, setError] = useState(null);
+    const [movieName, setMovieName] = useState('');
+    const [movies, setMovies] = useState([]);
+
+
+
     return (
         <div>
             <h1>Finder</h1>
+            <Form.Control type="text" placeholder="Search" value={movieName} onChange={(e) => setMovieName(e.target.value)} className="mt-3" />
+
         </div>
     )
 }
